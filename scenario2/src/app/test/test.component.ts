427c73bbaef456df070b5d0e900174c84076cd0a
vnikita0109 <vnikita793@gmail.com>
2019-06-19 00:35:30 +0530

    scenario2 achived successfully

---
+++ scenario2/src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+            <input [(ngModel)]="name" type="text">
+            {{name}}`,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  public name="";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
